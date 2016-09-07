6b5c46e225f28cb29701b02c6530e69935b52a6f
Fletcher Watson <fbwthe6@gmail.com>
2016-09-07 12:13:04 -0500

    completed a working version of this damn quiz.

--- dist/app.js
+++ dist/app.js
@@ -7,54 +7,58 @@
 // var attack = require("attack");
 let robots = require("./robots");
 
+let player1,
+player2,
+player1Robot,
+player2Robot,
+player1Name,
+player2Name;
+
 // $("#attack-btn").click(attackFn)
+$("#OK").on("click", function(){
+  player1Name = $("#player1-name").val();
+  player2Name = $("#player2-name").val();
+
+  player1Robot = $("#select-robot1").val();
+  player2Robot = $("#select-robot2").val();
+
+  player1 = new robots[player1Robot](player1Name);
+  player2 = new robots[player2Robot](player2Name);
 
 
-
-let playerStats = (player1, player2) => {
-
-  let player1Name = $("#player1-name").val(),
-      player2Name = $("#player2-name").val(),
-
-      player1Robot = $("#select-robot1").val(),
-      player2Robot = $("#select-robot2").val(),
-
-      user1 = new robots[player1Robot](),
-      user2 = new robots[player2Robot]();
-
-      user1.name = player1Name;
-      user2.name = player2Name;
-
-      console.log(user1.name);
-
-  if (!player1Name || !player2Name || !user1 || !user2) {
+  if (!player1Name || !player2Name || !player1 || !player2) {
     alert("Both y'all needa choose a name and a robot ya dingus.");
   }
 
-  console.log("Player 1 the", user1, "is named ", player1Name, ".", "Player 2 the", user2, "is named ", player2Name, ".");
-
-  player1 = `
-  <div class="output1">
-  <p>${user1.name} the ${user1.player1Robot}</p>
-  <div class="health">Health: ${user1.health}</div></div>
-  `;
-  player2 = `
-  <div class="output2">
-  <p>${user2.name} the ${user2.player2Robot}</p>
-  <div class="health">Health: ${user2.health}</div></div>
-`;
-
-  $("#battle-zone").append(player1, player2);
-
-};
-
+});
 $("#attack-btn").on("click", function(){
-  console.log("dogs");
-  playerStats();
+
+  // console.log("user1", user1, "user2", user2);
+
+player1.health -= player2.damage;
+player2.health -= player1.damage;
+  if (player1.health <= 0) {
+    alert(player2Name + " wins!");
+  } else if (player2.health <= 0) {
+    alert(player1Name + " wins!");
+  }
+
+  var player1Stats = `
+  <div class="output1">
+  <p>${player2.name} the ${player1Robot}</p>
+  <div class="health">Health: ${player1.health}</div></div>
+  `;
+  console.log("player1", player1.health);
+  var player2Stats = `
+  <div class="output2">
+  <p>${player2.name} the ${player2Robot}</p>
+  <div class="health">Health: ${player2.health}</div></div>
+`;
+  $("#battle-zone").append(player1Stats, player2Stats);
+
 });
 
 
-module.exports = playerStats;
 
 
 
